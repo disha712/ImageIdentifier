158edfc3f0b1fb300dc67cfb3ed4ff5ccce18e18
disha712 <76918036+disha712@users.noreply.github.com>
2022-09-12 19:41:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+    classifier=ml5.imageClassifier('DoodleNet');
+}
+function setup(){
+canvas=createCanvas(280,280);
+canvas.center();
+background("white");
+canvas.mouseReleased(classifyCanvas);
+synth=window.speechSynthesis;
+}
+function draw(){
+    strokeWeight(8);
+    stroke(0);
+    if (mouseIsPressed) {
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas,got_results);
+}
+function got_results(error,results){
+    if (error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById("label").innerHTML='label: '+results[0].label;
+    document.getElementById("confidence").innerHTML='confidence: '+Math.round(results[0].confidence*100)+"%";
+    utterThis=new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
+function clear_canvas(){
+    background("white");
+}
